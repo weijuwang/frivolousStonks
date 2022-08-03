24f239f34f65f2246252d396d904c06a94d93e9b
Weiju Wang <weijuwang@aol.com>
2022-08-03 15:35:45 -0700

    Updated .gitignore

---
+++ deployCommands.ts
@@ -0,0 +1,26 @@
+import { SlashCommandBuilder, Routes } from 'discord.js';
+import { REST } from '@discordjs/rest';
+
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/.env' });
+
+const rest = new REST({ version: '10' })
+	.setToken(process.env.DISCORD_TOKEN!);
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+]
+	.map(command => command.toJSON());
+
+// Add global commands
+rest
+	.put(Routes.applicationCommands(process.env.CLIENT_ID!), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
+/*
+// Delete all global commands
+rest.put(Routes.applicationCommands(process.env.CLIENT_ID!), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
+	.catch(console.error);
+*/
--- index.ts
+++ index.ts
@@ -1,15 +1,44 @@
 import * as Discord from 'discord.js';
 
-const client = new Discord.Client({
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/.env' });
+
+const client: Discord.Client = new Discord.Client({
     intents: [
         Discord.GatewayIntentBits.Guilds,
         Discord.GatewayIntentBits.GuildMembers,
-        Discord.GatewayIntentBits.GuildMessages
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent
     ]
 });
 
 client.on('ready', () => {
-    console.log("Add with https://discord.com/api/oauth2/authorize?client_id=1004486701177110708&permissions=2147485697&scope=bot");
+    console.log(`Add with https://discord.com/api/oauth2/authorize?client_id=${client!.user!.id}&permissions=2147485697&scope=bot`);
 });
+
+client.on('interactionCreate', async (interaction: Discord.Interaction) => {
+
+    if(!interaction.isChatInputCommand())
+        return;
+
+    switch(interaction.commandName){
+        case 'ping':
+            await interaction.reply('pong');
+            break;
+
+        default:
+            await interaction.reply(`Unrecognized command ${interaction.commandName}`);
+            break;
+    }
+});
+
+/*
+client.on('messageCreate', async (message: Discord.Message) => {
+
+    // Do not respond to bots
+    if(message.author.bot)
+        return;
+});
+*/
 
 client.login(process.env.DISCORD_TOKEN);
