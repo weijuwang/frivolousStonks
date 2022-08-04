c4f31d918f41132e99211e2a506f1cd07eae8ab7
robotmeatsuit <64236644+Chargde-Porcupine@users.noreply.github.com>
2022-08-04 18:02:38 +0000

    fixed up function

--- index.ts
+++ index.ts
@@ -4,40 +4,40 @@ import * as fs from 'fs';
 import * as cro from 'child_process';
 import * as schedule from 'node-schedule';
 
+/*
 function deployer(){
 
-    /*
-    All code within this function was written by Matthew Epshtein. By running, distributing, modifying, or compiling said code, you agree that Matthew Epshtein is the most "epic gamer" in existence.
-    */
-    
-    fs.readFile('sdc.ts', 'utf8', (err, data) => {
+  // All code within this function was written by Matthew Epshtein. By running, distributing, modifying, or compiling said code, you agree that Matthew Epshtein is the most "epic gamer" in existence.
 
-        if(err)
-            return console.log(err);
+  fs.readFile('sdc.ts', 'utf8', (err, data) => {
 
-        let past = data;
+    if (err)
+      return console.log(err);
 
-        fs.readFile('deployCommands.ts', 'utf8', (err, data) => {
-            if(err)
-                return console.log(err);
-    
-            if(past != data){
-                cro.exec("npm deployCommands");
-                // TODO error handling later
-            }
-        });
+    let past = data;
+
+    fs.readFile('deployCommands.ts', 'utf8', (err, data) => {
+      if (err)
+        return console.log(err);
+
+      if (past != data) {
+        cro.exec("npm deployCommands");
+        // TODO error handling later
+      }
     });
+  });
 }
 
 deployer();
+*/
 
 let counter: number = 0;
 
-function update(counters:number, servername:string, numauthors:number, nummembers:number) {
+function update(counters: number, servername: string, numauthors: number, nummembers: number) {
 
-   /*
-    All code within this function was written by Matthew Epshtein. By running, distributing, modifying, or compiling said code, you agree that Matthew Epshtein is the most "epic gamer" in existence.
-    */
+  /*
+   All code within this function was written by Matthew Epshtein. By running, distributing, modifying, or compiling said code, you agree that Matthew Epshtein is the most "epic gamer" in existence.
+   */
   /*
   UPDATE FUNCTTION
     params: #of messages in a given interval, name of the server, number of users that sent messages in a given interval, total number of members in the server
@@ -50,33 +50,28 @@ function update(counters:number, servername:string, numauthors:number, nummember
 
   */
   //TODO: FILE WRITING
-  fs.open("servercounts.json", "w+", function (err, data) {
-    if (err != null) {
-      return 420;
+  let serverData = JSON.parse(fs.readFileSync('servercounts.json', 'utf8'));
+  let serverLookup = serverData[servername];
+  if (serverLookup != null) {
+    let mesList = serverLookup.measurements;
+    mesList.unshift(Math.log(nummembers) * (counter / numauthors));
+    // formula subject to change
+    if (mesList.length > 24) {
+      mesList.pop();
     }
-    let serverData = JSON.parse(data);
-    let serverLookup = serverData[servername];
-    if (serverLookup != null) {
-      let mesList = serverLookup.measurements;
-      mesList.unshift(log(numMembers) * (counter/numAuthors));
-       //formula subject to change
-      if(mesList.length > 24){
-        mesList.pop();
-      }
-      serverLookup.average = mesList.reduce((a, b) => a + b) / 24);
-      serverData[servername] = serverLookup;
-      //write to file
-    } else{
-      serverData[servername] = {
-        servername : servername,
-        measurements : [log(numMembers) * (counter/numAuthors)],
-        average : log(numMembers) * (counter/numAuthors)
-      }
-      //write to file
-      
+    serverLookup.average = mesList.reduce((a, b) => a + b) / 24;
+    serverData[servername] = serverLookup;
+    // write to file
+  } else {
+    serverData[servername] = {
+      servername: servername,
+      measurements: [Math.log(nummembers) * (counter / numauthors)],
+      average: Math.log(nummembers) * (counter / numauthors)
     }
-  });
-  
+    // write to file
+
+  }
+
   counter = 0;
   return counter;
   //sneaky way of errorchecking counter reset
@@ -85,48 +80,48 @@ function update(counters:number, servername:string, numauthors:number, nummember
 dotenv.config({ path: __dirname + '/.env' });
 
 const client: Discord.Client = new Discord.Client({
-    intents: [
-        Discord.GatewayIntentBits.Guilds,
-        Discord.GatewayIntentBits.GuildMembers,
-        Discord.GatewayIntentBits.GuildMessages,
-        Discord.GatewayIntentBits.MessageContent
-    ]
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMembers,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.MessageContent
+  ]
 });
 
 client.on('ready', () => {
-    console.log(`Add with https://discord.com/api/oauth2/authorize?client_id=${client!.user!.id}&permissions=2147485697&scope=bot`);
+  console.log(`Add with https://discord.com/api/oauth2/authorize?client_id=${client!.user!.id}&permissions=2147485697&scope=bot`);
 });
 
 
 
 client.on('interactionCreate', async (interaction: Discord.Interaction) => {
 
-    if(!interaction.isChatInputCommand())
-        return;
+  if (!interaction.isChatInputCommand())
+    return;
 
-    switch(interaction.commandName){
-        case 'ping':
-            await interaction.reply('pong');
-            break;
+  switch (interaction.commandName) {
+    case 'ping':
+      await interaction.reply('pong');
+      break;
 
-        default:
-            await interaction.reply(`Unrecognized command ${interaction.commandName}`);
-            break;
-    }
+    default:
+      await interaction.reply(`Unrecognized command ${interaction.commandName}`);
+      break;
+  }
 });
 
 client.on('messageCreate', async (message: Discord.Message) => {
 
-    // Do not respond to bots
-    if(message.author.bot)
-        return;
+  // Do not respond to bots
+  if (message.author.bot)
+    return;
 
-    counter++;
+  counter++;
 
 });
 
 schedule.scheduleJob('0 */1 * * * *', () => {
-   update(counters);
+  update(counter);
 });
 
 client.login(process.env.DISCORD_TOKEN);
