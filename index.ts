4fcf367dd3e18f2a1b896c7b743474ccf3e0fd66
Weiju Wang <weijuwang@aol.com>
2022-08-04 11:54:26 -0700

    Fixed stock data updates

--- index.ts
+++ index.ts
@@ -4,11 +4,17 @@ import * as fs from 'fs';
 import * as cro from 'child_process';
 import * as schedule from 'node-schedule';
 
+const STOCKDATA = "stockData.json";
+
+interface ServerStockData {
+  data: number[],
+  average: number
+}
+
+/*
 function deployer(){
 
-    /*
-    All code within this function was written by Matthew Epshtein. By running, distributing, modifying, or compiling said code, you agree that Matthew Epshtein is the most "epic gamer" in existence.
-    */
+    // All code within this function was written by Matthew Epshtein. By running, distributing, modifying, or compiling said code, you agree that Matthew Epshtein is the most "epic gamer" in existence.
     
     fs.readFile('sdc.ts', 'utf8', (err, data) => {
 
@@ -30,57 +36,7 @@ function deployer(){
 }
 
 deployer();
-
-let counter: number = 0;
-
-function update(counters:number, servername:string, numauthors:number, nummembers:number) {
-
-   /*
-    All code within this function was written by Matthew Epshtein. By running, distributing, modifying, or compiling said code, you agree that Matthew Epshtein is the most "epic gamer" in existence.
-    */
-  /*
-  UPDATE FUNCTTION
-    params: #of messages in a given interval, name of the server, number of users that sent messages in a given interval, total number of members in the server
-    results: 
-      1.Updates the servercounts.json file with data passed into the function
-      2.clears the counter for messages
-    returns:
-      0 if everything went smoothly
-      !0 if problems occured
-
-  */
-  //TODO: FILE WRITING
-  fs.open("servercounts.json", "w+", function (err, data) {
-    if (err != null) {
-      return 420;
-    }
-    let serverData = JSON.parse(data);
-    let serverLookup = serverData[servername];
-    if (serverLookup != null) {
-      let mesList = serverLookup.measurements;
-      mesList.unshift(log(numMembers) * (counter/numAuthors));
-       //formula subject to change
-      if(mesList.length > 24){
-        mesList.pop();
-      }
-      serverLookup.average = mesList.reduce((a, b) => a + b) / 24);
-      serverData[servername] = serverLookup;
-      //write to file
-    } else{
-      serverData[servername] = {
-        servername : servername,
-        measurements : [log(numMembers) * (counter/numAuthors)],
-        average : log(numMembers) * (counter/numAuthors)
-      }
-      //write to file
-      
-    }
-  });
-  
-  counter = 0;
-  return counter;
-  //sneaky way of errorchecking counter reset
-}
+*/
 
 dotenv.config({ path: __dirname + '/.env' });
 
@@ -96,8 +52,6 @@ const client: Discord.Client = new Discord.Client({
 client.on('ready', async () => {
   console.log(`Add with https://discord.com/api/oauth2/authorize?client_id=${client!.user!.id}&permissions=2147485697&scope=bot`);
 });
-
-
 
 client.on('interactionCreate', async (interaction: Discord.Interaction) => {
 
@@ -118,16 +72,73 @@ client.on('interactionCreate', async (interaction: Discord.Interaction) => {
 
 client.on('messageCreate', async (message: Discord.Message) => {
 
-    // Do not respond to bots
-    if(message.author.bot)
-        return;
-
-    counter++;
-
+  // Do not respond to bots
+  if(message.author.bot)
+    return;
 });
 
-schedule.scheduleJob('0 */1 * * * *', () => {
-   update(counters);
+schedule.scheduleJob('0 * * * * *', () => {
+
+  // Read data
+  let serverData: {
+    [key: string]: ServerStockData
+  } = JSON.parse(fs.readFileSync(STOCKDATA).toString());
+
+  // TODO "For each server the bot can see..."
+  {
+    // TODO get this data from the actual server
+    let serverId: string = "12345";
+    let numMessages: number = 100; // in the last hour
+    let numAuthors: number = 20; // in the last hour
+    let numMembers: number = 50; // in the server right now
+
+    // All code within this function was written by Matthew Epshtein. By running, distributing, modifying, or compiling said code, you agree that Matthew Epshtein is the most "epic gamer" in existence.
+  
+    /*
+    UPDATE FUNCTION
+      params: # of messages in a given interval, name of the server, number of users that sent messages in a given interval, total number of members in the server
+      results: 
+        1.Updates the servercounts.json file with data passed into the function
+        2.clears the counter for messages
+      returns:
+        0 if everything went smoothly
+        !0 if problems occured
+    */
+    //TODO: FILE WRITING
+
+    let thisServer = serverData[serverId];
+
+    if(thisServer != null){
+
+      let newData = thisServer.data;
+
+      // Add data from the last hour
+      newData.unshift(Math.log(numMembers) * (numMessages / numAuthors));
+
+      // Remove the oldest data point (from exactly 24 hours ago)
+      if(newData.length > 24)
+        newData.pop();
+
+      thisServer.data = newData;
+
+      // Compute the average of all 24 data points
+      thisServer.average = newData.reduce((a: number, b: number) => a + b) / 24;
+
+      serverData[serverId] = thisServer;
+
+    } else {
+      // This is the first time we've collected data from this server
+      const firstDataPoint = Math.log(numMembers) * (numMessages / numAuthors);
+
+      serverData[serverId] = {
+        data: [firstDataPoint],
+        average: firstDataPoint
+      };
+    }
+  }
+
+  // Write data back to the file
+  fs.writeFileSync(STOCKDATA, JSON.stringify(serverData));
 });
 
 client.login(process.env.DISCORD_TOKEN);
